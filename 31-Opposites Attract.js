f6b121294c670aa513d239ef69c5530bf60b89d1
fikrifaizalha <fikrifaizalha@gmail.com>
2024-05-21 14:54:15 +0700

    codewars

---
+++ 31-Opposites Attract.js
@@ -0,0 +1,11 @@
+// Fikri Faizal Hamzah Alfa'iz //
+
+// Codewars Challenge 2024 //
+
+// https://www.codewars.com/kata/555086d53eac039a2a000083
+
+function lovefunc(flower1, flower2) {
+  return flower1 % 2 !== flower2 % 2;
+}
+
+console.log(lovefunc(1, 4));
